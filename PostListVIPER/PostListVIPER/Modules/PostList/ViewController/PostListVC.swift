bfe9186b2a35437682c277eaf714979740545b57
iOS991 <ghoshsajib5s@gmail.com>
2024-04-03 13:59:51 +0530

    setup project

---
+++ PostListVIPER/PostListVIPER/Modules/PostList/ViewController/PostListVC.swift
@@ -0,0 +1,27 @@
+//
+//  PostListVC.swift
+//  PostListVIPER
+//
+//  Created by Sajib Ghosh on 03/04/24.
+//
+
+import UIKit
+
+class PostListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
